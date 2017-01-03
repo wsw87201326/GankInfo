c46592a8aab0166a55bb370acbb1f7257bca3a10
wsw87201326 <243633732@qq.com>
2017-01-03 19:56:03 +0800

    Net Exception to Define

--- app/src/main/java/com/wsw/gankinfo/net/RequestType.java
+++ app/src/main/java/com/wsw/gankinfo/net/RequestType.java
@@ -1,9 +1,36 @@
 package com.wsw.gankinfo.net;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * 请求类型 Created by wsw on 2016/12/6.
  */
 @SuppressWarnings("unused")
-public enum RequestType {
-    福利, Android, iOS, 休息视频, 拓展资源, 前端, all
+public class RequestType {
+    public static final int 福利 = 0x01;
+    public static final int Android = 0x02;
+    public static final int iOS = 0x03;
+    public static final int 休息视频 = 0x04;
+    public static final int 拓展资源 = 0x05;
+    public static final int 前端 = 0x06;
+    public static final int all = 0x07;
+
+    @IntDef({福利, Android, iOS, 休息视频, 拓展资源, 前端, all})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface RequestTypes {
+    }
+
+    private int userType;
+
+    @RequestTypes
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(@RequestTypes int userType) {
+        this.userType = userType;
+    }
 }
---
+++ app/src/main/java/com/wsw/gankinfo/net/exception/ApiException.java
@@ -0,0 +1,38 @@
+package com.wsw.gankinfo.net.exception;
+
+/**
+ * Created by Administrator on 2017/1/3.
+ */
+
+public class ApiException extends Exception {
+    /*错误码*/
+    private int code;
+    /*显示的信息*/
+    private String displayMessage;
+
+    public ApiException(Throwable e) {
+        super(e);
+    }
+
+    public ApiException(Throwable cause,@CodeException.CodeEp int code, String showMsg) {
+        super(showMsg, cause);
+        setCode(code);
+        setDisplayMessage(showMsg);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDisplayMessage() {
+        return displayMessage;
+    }
+
+    public void setDisplayMessage(String displayMessage) {
+        this.displayMessage = displayMessage;
+    }
+}
